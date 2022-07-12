a4062fad8e3091322bb5865a6ea880b802230a6c
sebih salim <htc.sse1221@gmail.com>
2022-07-12 15:12:02 +0200

    La création de listes dans scala

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,19 +1,37 @@
+import scala.collection.mutable._
+
+
+
 object HelloWorld {
   /*
   premier projet git avec intellij/scala
    */
   val ma_var_immutable : String = "Salim" // variable immutable à portée privée
   private val une_var_immutable : String= "mon test de scala"
+
+
+
+
   def main(args: Array[String]): Unit = {
 
     println("Hello World : It's my first project, I am happy")
+
     var test:Int = 15
+
     test = test +10
+
     println(test)
+
     println(comptage_caracteres("salut les gars"))
+
     getResult(10)
+
     testWhile(5)
+
     tesFor()
+
+    collectionScala()
+
   }
 /*
 Ma première fonction
@@ -58,5 +76,20 @@ def comptage_caracteres
       println("itération for N° "+i)
 
     }
+
+
+    }
+
+
+  // création de List adns scala
+  def collectionScala(): Unit = {
+    val ma_liste: List[Int] = List(1, 2, 3, 5, 45, 15)
+    val liste_s: List[String] = List("Salim", "Sarah", "Manel")
+    val chaine: String = "ma chaine"
+    val plage_v: List[Int] = List.range(1, 15, 2)
+    println(ma_liste(0))
+    println(plage_v(2))
+    println(plage_v)
   }
+
 }
