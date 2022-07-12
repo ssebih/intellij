17d8dff329e5946e207cd95f7ba422e11c8100f6
sebih salim <htc.sse1221@gmail.com>
2022-07-12 11:29:38 +0200

    premiere boucle while avec condition

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -10,13 +10,43 @@ object HelloWorld {
     var test:Int = 15
     test = test +10
     println(test)
-    println(comptage_caracteres("Bonjour je suis salim"))
+    println(comptage_caracteres("salut les gars"))
+    getResult(10)
+    testWhile(5)
   }
 /*
 Ma première fonction
 def comptage_caracteres
  */
   def comptage_caracteres (text : String) : Int = {
-  return (text.trim().length)
+    if (text.isEmpty){
+      return 0
+    }
+    else{
+      return (text.trim.length )
+    }
+
+  }
+  /*
+Ma première procedure (methode)
+def comptage_caracteres
+ */
+  def getResult (parametre : Any): Unit ={
+    if (parametre== 10){
+      println("votre valeur est un entier")}
+      else {
+        println("votre valeur est une chaine de caractere")
+      }
+
+
+  }
+  // boucles
+  def testWhile (valeur_conditionnelle: Int): Unit ={
+    var i: Int = 0
+    while (i< valeur_conditionnelle){
+      println("itération while N° "+i)
+      i=i+1
+    }
+
   }
 }
