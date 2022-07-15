fb74ccedd8af98c5b923a146a9c2f99dfac5bb82
sebih salim <htc.sse1221@gmail.com>
2022-07-15 10:37:01 +0200

    développement d'un parallelize

--- src/main/scala/SparkBigData.scala
+++ src/main/scala/SparkBigData.scala
@@ -1,34 +1,51 @@
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SparkSession._
 
-object SparkBigData {
-  var ss: SparkSession= null
+
+
+
+
+
+object SparkBigDataSession {
+  var ss : SparkSession= null
+
+  def main(args: Array[String]): Unit = {
+    val sparkss= sessionSpark(true).sparkContext
+    val rddTest : RDD[String] = sparkss.parallelize(List("Salim","Sarah","Manel"))
+    rddTest.foreach(e => println(e))
+  }
+
+
   /**
-   * fonction qui initialise et instancie une session spark
-   * @param Env: c'est une variable qui indique l'environnement de l'éxecution de spark app:
-   *           -env = true ==> local
-   *           -env = false ==> cluster
+   * initilisation d'une session spark
+   * @param env : indique l'environnement du déploiement de lapp spark :
+   *              -1 si env = true ==> en local
+   *              -2 sinon déploiement sur cluster
    */
 
-  def sparkSessiion2(env : Boolean = true) {
-
-    if (env == true){
-      System.setProperty("hadoop.home.dir","C:/install/exe&zip")
+  def sessionSpark (env : Boolean = true) : SparkSession ={
+    if (env== true){
+      System.setProperty("hadoop.home.dir","C:/install/exe&zip/hadoop")
       ss = SparkSession.builder()
-          .master("local")
-          .config("spark.sql.crossJoin.enabled", "true")
+          .master(("local[*]"))
           .enableHiveSupport()
           .getOrCreate()
     }else{
-      val ss: SparkSession = SparkSession.builder()
-        .appName("spark-application")
-        .config("spark.serializer", "org.apache.spark.serializer.kryoSerializer")
+       ss = SparkSession.builder()
+        .appName("SAPRK_SALIM")
+        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         .config("spark.sql.crossJoin.enabled", "true")
-        .enableHiveSupport()
-        .getOrCreate()
+        .enableHiveSupport().getOrCreate()
+
     }
 
+ss
+
   }
+
+
+
 
 }
